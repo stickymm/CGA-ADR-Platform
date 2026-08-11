9a967c792e7187f5d203faf62de78ebcb5b9ab8c
c00l-crypto-91 <colist2121@gmail.com>
2026-08-11 14:46:13 -0400

    fixing TurnTest

--- navigation/missions/TurnTest.py
+++ navigation/missions/TurnTest.py
@@ -3,7 +3,11 @@ class testMission(GateMission):
     def run(self, nav: NavigationController):
         gateNum : int = 1
         gate_count = 0
+
+        print("starting turn test")
+
         while nav.running and gate_count < gateNum:
+            print("beginning the search")
             gate = self.observe_gate(nav, duration = 3.0)
             nav.turn_test(gate, 3.0)
         
