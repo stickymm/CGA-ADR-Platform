b112d771eaf43f45ea807d9ea03bb727cd18ab8d
c00l-crypto-91 <colist2121@gmail.com>
2026-08-11 14:49:30 -0400

    new turntest edit (added in some checks)

--- navigation/missions/TurnTest.py
+++ navigation/missions/TurnTest.py
@@ -1,11 +1,15 @@
-from Colins_Nav import GateMission, NavigationController
+from Colins_Nav import GateMission, NavigationController, GateDetection
 class testMission(GateMission):
     def run(self, nav: NavigationController):
         gateNum : int = 1
         gate_count = 0
 
         print("starting turn test")
+        gate1 = GateDetection(0.0, 0.0, 0.0, 0.0, -2.0, 0.0, 0.0, 0.0)
 
+        target1 = self.build_standoff_target(nav, gate1, 1.0)
+        nav.move_to_target(target1, "running")
+        
         while nav.running and gate_count < gateNum:
             print("beginning the search")
             gate = self.observe_gate(nav, duration = 3.0)
